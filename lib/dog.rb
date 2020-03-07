846d4b700d6cbb24db4dde43ed678cab1221d36d
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-07 03:15:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,9 +6,16 @@ class Dog
   
   def initialize(name)
   @name = name
+<<<<<<< HEAD
   
   save
   
+=======
+  @@all << self
+  save
+  
+
+>>>>>>> 552c099aca923ee3580e9f61b5465e9694df72c3
   end
 
   def self.all
@@ -20,11 +27,21 @@ class Dog
   end
   
   
+<<<<<<< HEAD
   def self.print_all
   self.all.each {|dog| puts "#{dog.name}"}
   end
   
   def save
+=======
+  
+  def self.print_all
+  self.all.each {|dog| puts "#{dog.name}"}
+  
+  end
+  
+  def save 
+>>>>>>> 552c099aca923ee3580e9f61b5465e9694df72c3
     @@all << self
   end
   
